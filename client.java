c8d2f2c7f2bde33a17ba498ac7648bdb2e8bd3b3
Disha Singh <48053259+D1sha-Singh@users.noreply.github.com>
2021-06-17 13:12:50 +0530

    Create client.java

---
+++ client.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author Asus
+ */
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class client {
+
+public static void main(String args[])
+{
+    
+    Game t=new Game();
+    JOptionPane.showMessageDialog(t,"Invalid Move");
+}
+}
